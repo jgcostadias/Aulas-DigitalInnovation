0e67a31e50827fae7f6f06f938762b224c2eaac9
unknown <jgcosta.dias@gmail.com>
2021-03-31 18:51:41 -0300

    Desafio

---
+++ Desafio Basico/Pimentao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Pimentao 
+{  
+    public static void Main() 
+    {
+      
+      int a,b,t,x;
+      int opcao;
+
+
+      Console.WriteLine ("1 - Pimentão vermelho:");
+      Console.WriteLine ("2 - Pimentão amarelo:");
+      Console.WriteLine ("3 - Total:");
+      Console.WriteLine ("4 - Sair");
+
+      
+      while
+      {
+        double notaEntradaConsole = double.Parse(Console.ReadLine());
+        
+        if (notaEntradaConsole < 0 || notaEntradaConsole > 10)
+        {
+          Console.WriteLine("nota invalida");
+        } 
+        else 
+        {
+          contador++;
+          
+          somaDasNotas += notaEntradaConsole; 
+        }    
+      } 
+      while (contador < 2);
+      
+      Console.WriteLine("media = " + (somaDasNotas / 2).ToString("N2"));
+    }
+}
